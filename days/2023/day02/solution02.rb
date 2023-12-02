8683e1b4a67c1fa0e164297fdd4b9a4ebdbfd77b
Steve Whittaker <swhitt@gmail.com>
2023-12-01 20:56:29 -0600

    Setup for Day 2

---
+++ days/2023/day02/solution02.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../../../lib/base"
+
+class Solution02 < Base
+  def part_1
+    # do it first
+  end
+
+  def part_2
+    # do it second
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  solution = Solution02.new
+  # rubocop:disable Lint/Debugger
+  binding.pry
+  # rubocop:enable Lint/Debugger
+end
