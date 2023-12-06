e8e9f9844032874ad183f3f4f8f39071325f0396
Steve Whittaker <swhitt@gmail.com>
2023-12-06 10:09:45 -0600

    Day 6 - Simplify

--- 2023/06/solution06.rb
+++ 2023/06/solution06.rb
@@ -3,22 +3,17 @@ require_relative "../../lib/base"
 class AoC::Year2023::Solution06 < Base
   def part1
     times, distances = input_lines.map { |line| line.scan(/\d+/).map(&:to_i) }
-    calculate_ways_to_win(times.zip(distances))
+    calculate_wins(times.zip(distances))
   end
 
   def part2
-    input_time, input_distance = input_lines.map { |line| line.gsub(/[^\d]/, "").to_i }
-    calculate_ways_to_win([[input_time, input_distance]])
+    calculate_wins([input_lines.map { |line| line.delete("^0-9").to_i }])
   end
 
   private
 
-  def calculate_ways_to_win(races)
-    races.inject(1) { |product, (time, record)| product * calculate_ways(time, record) }
-  end
-
-  def calculate_ways(time, record_distance)
-    (1...time).count { |hold_duration| (time - hold_duration) * hold_duration > record_distance }
+  def calculate_wins(races)
+    races.map { |time, distance| (1...time).count { |hold| (time - hold) * hold > distance } }.reduce(:*)
   end
 end
 
