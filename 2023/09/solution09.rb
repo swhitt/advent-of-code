06c840fd450feb80aa78a5449ea2b99ed7f076c5
Steve Whittaker <swhitt@gmail.com>
2023-12-09 20:03:36 -0600

    Simplify day 9

--- 2023/09/solution09.rb
+++ 2023/09/solution09.rb
@@ -2,20 +2,15 @@ require_relative "../../lib/base"
 
 class AoC::Year2023::Solution09 < Base
   def part1
-    parsed_histories.sum { |history| predict_value(history, :next) }
+    parsed_histories.sum { |history| predict_next(history) }
   end
 
   def part2
-    parsed_histories.sum { |history| predict_value(history, :previous) }
+    parsed_histories.sum { |history| predict_next(history.reverse) }
   end
 
   def parsed_histories
     @parsed_histories ||= input_lines.map { |line| line.split.map(&:to_i) }
-  end
-
-  def predict_value(history, direction)
-    sequences = generate_difference_sequences(history)
-    (direction == :next) ? predict_next(sequences) : predict_previous(sequences)
   end
 
   def generate_difference_sequences(sequence)
@@ -26,21 +21,11 @@ class AoC::Year2023::Solution09 < Base
     sequences
   end
 
-  def predict_next(sequences)
+  def predict_next(history)
+    sequences = generate_difference_sequences(history)
     next_value = sequences.first.last
     sequences.reverse_each { |seq| next_value += seq.last if seq.length != sequences.first.length }
     next_value
-  end
-
-  def predict_previous(sequences)
-    difference_sequences = sequences
-    difference_sequences.last.unshift(0)
-
-    (difference_sequences.size - 2).downto(0) do |i|
-      difference_sequences[i].unshift(difference_sequences[i].first - difference_sequences[i + 1].first)
-    end
-
-    difference_sequences.first.first
   end
 end
 
