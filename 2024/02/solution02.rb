a37bd62a754da82d7fa70ed29d4198207f428f2e
Steve Whittaker <swhitt@gmail.com>
2024-12-01 23:18:04 -0600

    2024 - day 2

---
+++ 2024/02/solution02.rb
@@ -0,0 +1,30 @@
+require_relative "../../lib/base"
+
+# Solution for the 2024 day 2 puzzle
+# https://adventofcode.com/2024/day/2
+class AoC::Year2024::Solution02 < Base
+  def part1
+    reports.count { safe_report?(_1) }
+  end
+
+  def part2
+    reports.count { safe_with_problem_dampener?(_1) }
+  end
+
+  def reports
+    input_lines.map { _1.split.map(&:to_i) }
+  end
+
+  def safe_report?(report)
+    diffs = report.each_cons(2).map { |a, b| b - a }
+    diffs.all? { |d| d.abs <= 3 } && (diffs.all?(&:positive?) || diffs.all?(&:negative?))
+  end
+
+  def safe_with_problem_dampener?(report)
+    return true if safe_report?(report)
+
+    report.each_index.any? do |i|
+      safe_report?(report[0...i] + report[i + 1..])
+    end
+  end
+end
