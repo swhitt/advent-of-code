537e52307732255ddfd5c7c72717e7697ef988c6
Steve Whittaker <swhitt@gmail.com>
2024-12-08 18:27:05 -0600

    in-line some stuff, conciseify

--- 2024/08/solution08.rb
+++ 2024/08/solution08.rb
@@ -26,34 +26,23 @@ class AoC::Year2024::Solution08 < Base
   end
 
   def find_part1_antinode_positions(nodes, dimensions)
-    nodes.each_value.with_object(Set.new) do |positions, antinodes|
+    nodes.values.each_with_object(Set.new) do |positions, antinodes|
       positions.combination(2).flat_map { |a, b| [[a, b], [b, a]] }.each do |from, to|
-        candidate = extrapolate_position(from, to)
-        antinodes << candidate if valid_position?(candidate, dimensions)
+        candidate = from.zip(to).map { |a, b| 2 * b - a }
+        antinodes << candidate if candidate.zip(dimensions).all? { |coord, limit| coord.between?(0, limit - 1) }
       end
     end
   end
 
-  def extrapolate_position(from, to)
-    from.zip(to).map! { |a, b| 2 * b - a }
-  end
-
-  def valid_position?(pos, dimensions)
-    pos.zip(dimensions).all? { |coord, limit| (0...limit).cover?(coord) }
-  end
-
   def find_part2_antinode_positions(nodes, dimensions)
-    antinodes = Set.new
-    nodes.each do |_freq, positions|
-      next if positions.size < 2
+    nodes.values.select { |pos| pos.size >= 2 }.each_with_object(Set.new) do |positions, antinodes|
+      # find lines from at least two antennas of the same frequency
       line_map = Hash.new { |h, k| h[k] = Set.new }
 
-      # lines from at least two antennas of the same frequency
       positions.combination(2).each do |(x1, y1), (x2, y2)|
         dx, dy = x2 - x1, y2 - y1
         g = dx.gcd(dy)
-        dx /= g
-        dy /= g
+        dx, dy = dx.fdiv(g), dy.fdiv(g)
         dx, dy = -dx, -dy if dx < 0 || (dx == 0 && dy < 0)
 
         # perpendicular normal
@@ -68,26 +57,22 @@ class AoC::Year2024::Solution08 < Base
         antinodes.merge(all_points_on_line(ants.first, dx, dy, dimensions))
       end
     end
-    antinodes
   end
 
   def all_points_on_line(base, dx, dy, dimensions)
     x0, y0 = base
     max_x, max_y = dimensions
-    t_min_x, t_max_x = dx.zero? ? [-Float::INFINITY, Float::INFINITY] : bounds_for_t(x0, dx, max_x)
-    t_min_y, t_max_y = dy.zero? ? [-Float::INFINITY, Float::INFINITY] : bounds_for_t(y0, dy, max_y)
 
+    t_bounds = ->(coord, delta, max) {
+      return [-Float::INFINITY, Float::INFINITY] if delta.zero?
+      [(max - 1 - coord) / delta.to_f, -coord / delta.to_f].sort
+    }
+
+    t_min_x, t_max_x = t_bounds[x0, dx, max_x]
+    t_min_y, t_max_y = t_bounds[y0, dy, max_y]
     t_min = [t_min_x, t_min_y].max.ceil
     t_max = [t_max_x, t_max_y].min.floor
 
     (t_min..t_max).map { |t| [x0 + t * dx, y0 + t * dy] }
-  end
-
-  def bounds_for_t(start, d, limit)
-    if d > 0
-      [-start.to_f / d, (limit - 1 - start).to_f / d]
-    else
-      [(limit - 1 - start).to_f / d, -start.to_f / d]
-    end
   end
 end
