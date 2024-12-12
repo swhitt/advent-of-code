548aa3519e0b9f2c0ce954c5b0c790515dd9624a
Steve Whittaker <swhitt@gmail.com>
2024-12-12 09:13:28 -0600

    2024 Day 12

---
+++ 2024/12/solution12.rb
@@ -0,0 +1,81 @@
+require_relative "../../lib/base"
+
+# Solution for the 2024 day 12 puzzle
+# https://adventofcode.com/2024/day/12
+class AoC::Year2024::Solution12 < Base
+  DIRECTIONS = [
+    Point[0, -1],
+    Point[0, 1],
+    Point[-1, 0],
+    Point[1, 0]
+  ].freeze
+
+  def part1 = find_regions.sum { |region| region.size * count_external_edges(region) }
+
+  def part2 = find_regions.sum { |region| region.size * count_unique_sides(region) }
+
+  private
+
+  def find_regions
+    seen = Set.new
+    grid = parse_grid
+
+    grid.keys.filter_map do |point|
+      next if seen.include?(point)
+      region = flood_fill(point, grid, seen)
+      region if region.any?
+    end
+  end
+
+  def flood_fill(start, grid, seen)
+    region = Set.new
+    queue = Queue.new
+    queue << start
+
+    until queue.empty?
+      current = queue.pop
+      next if region.include?(current)
+
+      region << current
+      seen << current
+
+      DIRECTIONS.each do |dir|
+        next_point = current + dir
+        queue << next_point if grid[next_point] == grid[current]
+      end
+    end
+
+    region
+  end
+
+  def count_external_edges(region)
+    region.sum { |point| DIRECTIONS.count { |dir| !region.include?(point + dir) } }
+  end
+
+  def count_unique_sides(region)
+    sides = Set.new
+
+    region.each do |point|
+      DIRECTIONS.each do |dir|
+        next if region.include?(point + dir)
+
+        current = point
+        walk_dir = Point[-dir.y, dir.x]
+
+        while region.include?(current + walk_dir) && !region.include?(current + dir + walk_dir)
+          current += walk_dir
+        end
+
+        sides << [current, dir]
+      end
+    end
+
+    sides.size
+  end
+
+  def parse_grid
+    input_lines.each_with_index.flat_map do |line, y|
+      line.chars.each_with_index.map { |char, x| [Point[x, y], char] }
+    end.to_h
+  end
+end
