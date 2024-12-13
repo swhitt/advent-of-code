7ece4aff8d4c1841f5645091c82cd58e4ca126b3
Steve Whittaker <swhitt@gmail.com>
2024-12-13 08:59:32 -0600

    refactor / optimize

--- 2024/13/solution13.rb
+++ 2024/13/solution13.rb
@@ -8,55 +8,43 @@ class AoC::Year2024::Solution13 < Base
       # a * button_a + b * button_b = prize + offset
       ax, ay = button_a
       bx, by = button_b
-      px, py = prize[0] + offset, prize[1] + offset
+      px, py = prize.map { _1 + offset }
 
       det = ax * by - ay * bx
-      return nil if det.zero?
+      return if det.zero?
 
       a = (px * by - py * bx) / det.to_f
       b = (ax * py - ay * px) / det.to_f
 
-      return nil unless a.round == a && b.positive? && b.round == b && b.positive?
+      return unless [a, b].all? { _1.positive? && _1.round == _1 }
 
       a, b = a.to_i, b.to_i
-      x = a * ax + b * bx
-      y = a * ay + b * by
 
-      return nil unless x == px && y == py
+      return unless [a * ax + b * bx, a * ay + b * by] == [px, py]
 
-      (a * 3) + b
+      a * 3 + b
     end
   end
+
+  def part1 = machines.sum { _1.solve_minimum_tokens || 0 }
+
+  def part2 = machines.sum { _1.solve_minimum_tokens(offset: 10_000_000_000_000) || 0 }
 
   def machines
-    machines = []
-    current = {}
+    input.each_line(chomp: true)
+      .slice_before(/^Button A/)
+      .filter_map do |chunk|
+      nums = chunk.join
+        .scan(/(?:X\+|X=|Y\+|Y=)(\d+)/)
+        .map { _1[0].to_i }
 
-    input.each_line(chomp: true) do |line|
-      next if line.empty?
+      next unless nums.size == 6
 
-      case line
-      when /^Button A: X\+(\d+), Y\+(\d+)$/
-        current[:button_a] = [$1.to_i, $2.to_i]
-      when /^Button B: X\+(\d+), Y\+(\d+)$/
-        current[:button_b] = [$1.to_i, $2.to_i]
-      when /^Prize: X=(\d+), Y=(\d+)$/
-        current[:prize] = [$1.to_i, $2.to_i]
-        if current.size == 3
-          machines << Machine.new(current[:button_a], current[:button_b], current[:prize])
-          current = {}
-        end
-      end
+      button_a = nums[0..1]
+      button_b = nums[2..3]
+      prize = nums[4..5]
+
+      Machine.new(button_a, button_b, prize)
     end
-
-    machines
-  end
-
-  def part1
-    machines.sum { _1.solve_minimum_tokens || 0 }
-  end
-
-  def part2
-    machines.sum { _1.solve_minimum_tokens(offset: 10_000_000_000_000) || 0 }
   end
 end
